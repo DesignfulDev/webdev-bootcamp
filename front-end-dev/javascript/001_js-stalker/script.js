44984ceaa15e153c91a7c5bfb03de4d19d46d328
Maurice Lacerda <lacerda@protonmail.com>
2018-01-20 12:32:26 -0200

    first tries on JavaScript

---
+++ front-end-dev/javascript/001_js-stalker/script.js
@@ -0,0 +1,6 @@
+var usrFirstName = prompt("Please, enter your first name:");
+var usrLastName = prompt("Now enter your last name:");
+var usrAge = prompt("How old are you?");
+
+console.log("The user's name is " + usrFirstName + " " + usrLastName);
+console.log("The user is " + usrAge + " years old");
---
+++ front-end-dev/javascript/002_age-calculator/script.js
@@ -0,0 +1,3 @@
+var usrAgeYears = prompt("How old are you?");
+var usrAgeDays = usrAgeYears * 365.25;
+alert(usrAgeYears + " years is roughly " + usrAgeDays + " days");
